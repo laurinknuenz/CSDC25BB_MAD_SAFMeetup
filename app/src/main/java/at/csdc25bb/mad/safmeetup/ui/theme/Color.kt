89c7c48be9dd07ae934802383b8ccb33c471b580
laurinknuenz <114426294+laurinknuenz@users.noreply.github.com>
2024-06-07 13:55:37 +0200

    Adapts MaterialUI color scheme to use project colors

--- app/src/main/java/at/csdc25bb/mad/safmeetup/ui/theme/Color.kt
+++ app/src/main/java/at/csdc25bb/mad/safmeetup/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val darkOrange = Color(0xFFFF930E)
+val lightOrange = Color(0xFFFFEDD8)
+val triadGreen = Color(0xFF0eff93)
--- app/src/main/java/at/csdc25bb/mad/safmeetup/ui/theme/Theme.kt
+++ app/src/main/java/at/csdc25bb/mad/safmeetup/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkOrange,
+    secondary = lightOrange,
+    tertiary = triadGreen
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = darkOrange,
+    secondary = lightOrange,
+    tertiary = triadGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -39,9 +39,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun SportsAndFriendsMeetupTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false, //isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, //true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
